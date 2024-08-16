aade96f02dd1a2cdabfb27edee884c81bb259a8d
Gloriah-B <Gloriah-B@github.com>
2024-08-16 18:29:09 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -7,39 +7,27 @@ This module provides a Server class to paginate database of popular baby names
 import csv
 from typing import List, Tuple
 
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Calculate the start and end index for pagination.
-
-    Args:
-        page (int): The current page number (1-indexed).
-        page_size (int): The number of items per page.
-
-    Returns:
-        Tuple[int, int]: A tuple containing the start and end indices.
-    """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return start_index, end_index
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initialize instance."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
+        """Cached dataset.
+
+        Loads the dataset from a CSV file if it hasn't been loaded yet.
         """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
+            self.__dataset = dataset[1:]  # Exclude header row
 
         return self.__dataset
 
@@ -62,6 +50,7 @@ class Server:
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
 
+        # Return an empty list if the start index is out of range
         if start_index >= len(dataset):
             return []
 
