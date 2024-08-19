064a3267a410c848b17a3ccc2d999061b010e445
Gloriah-B <Gloriah-B@github.com>
2024-08-19 19:58:51 +0000

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -5,6 +5,7 @@ This module provides a Server class to paginate database of popular baby names
 
 
 import csv
+import os
 from typing import List, Tuple
 
 index_range = __import__('0-simple_helper_function').index_range
@@ -12,7 +13,7 @@ index_range = __import__('0-simple_helper_function').index_range
 
 class Server:
     """Server class to paginate a database of popular baby names."""
-    DATA_FILE = "Popular_Baby_Names.csv"
+    DATA_FILE = 'Popular_Baby_Names.csv'
 
     def __init__(self):
         """Initialize instance."""
@@ -24,11 +25,19 @@ class Server:
         Loads the dataset from a CSV file if it hasn't been loaded yet.
         """
         if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # Exclude header row
+            if not os.path.exists(self.DATA_FILE):
+                raise FileNotFoundError(f"File not found: {self.DATA_FILE}")
 
+            try:
+                with open(self.DATA_FILE, mode='r') as f:
+                    reader = csv.reader(f)
+                self.__dataset = [row for row in reader][1:]
+            except FileNotFoundError:
+                raise FileNotFoundError(
+                            f"Could not open file: {self.DATA_FILE}"
+                                        )
+            except Exception as e:
+                raise Exception(f"An error occurred while reading file:{e}")
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
@@ -42,10 +51,12 @@ class Server:
         Returns:
             List[List]: The appropriate page of the dataset.
         """
-        assert isinstance(page, int) and page > 0,
-        "Page must be a positive integer."
-        assert isinstance(page_size, int) and page_size > 0,
-        "Page size must be a positive integer."
+        assert isinstance(page, int) and page > 0, (
+                                            "Page must be a positive integer."
+                                                    )
+        assert isinstance(page_size, int) and page_size > 0, (
+                                    "Page size must be a positive integer."
+                                                            )
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
